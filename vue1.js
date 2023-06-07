f6c40255eee5a05ee1fe1cbf670f60c7ecbbe761
San <san@mjvn.com>
2023-06-07 15:06:36 +0200

    Update: Improve my skillset

---
+++ vue1.js
@@ -0,0 +1,131 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            skillset: [
+                {
+                  "skill": "Linux Mint Operating System",
+                  "comfortability": 70
+                },
+                {
+                  "skill": "Windows Operating System",
+                  "comfortability": 90
+                }
+                ,
+                {
+                  "skill": "MySQL",
+                  "comfortability": 50
+                }
+                ,
+                {
+                  "skill": "PostgreSQL",
+                  "comfortability": 50
+                }
+                ,
+                {
+                  "skill": "Quantum GIS",
+                  "comfortability": 55
+                }
+                ,
+                {
+                  "skill": "XAMPP",
+                  "comfortability": 90
+                }
+                ,
+                {
+                  "skill": "MySQL Workbench",
+                  "comfortability": 90
+                }
+                ,
+                {
+                  "skill": "DBeaver",
+                  "comfortability": 70
+                }
+                ,
+                {
+                  "skill": "C Language",
+                  "comfortability": 50
+                }
+                ,
+                {
+                  "skill": "C++",
+                  "comfortability": 50
+                }
+                ,
+                {
+                  "skill": "Python",
+                  "comfortability": 50
+                }
+                ,
+                {
+                  "skill": "Java",
+                  "comfortability": 50
+                }
+                ,
+                {
+                  "skill": "PHP",
+                  "comfortability": 72
+                }
+                ,
+                {
+                  "skill": "Laravel",
+                  "comfortability": 70
+                }
+                ,
+                {
+                  "skill": "JavaScript",
+                  "comfortability": 50
+                }
+                ,
+                {
+                  "skill": "JQuery",
+                  "comfortability": 55
+                }
+                ,
+                {
+                  "skill": "AJAX",
+                  "comfortability": 50
+                }
+                ,
+                {
+                  "skill": "HTML",
+                  "comfortability": 95
+                }
+                ,
+                {
+                  "skill": "CSS",
+                  "comfortability": 60
+                }
+                ,
+                {
+                  "skill": "Bootstrap",
+                  "comfortability": 80
+                }
+                ,
+                {
+                  "skill": "Wix-Velo",
+                  "comfortability": 50
+                }
+                ,
+                {
+                  "skill": "Maple",
+                  "comfortability": 80
+                }
+                ,
+                {
+                  "skill": "Postman",
+                  "comfortability": 50
+                }
+                ,
+                {
+                  "skill": "Laravel Compass",
+                  "comfortability": 80
+                }
+                ,
+                {
+                  "skill": "Apache2, NGINX, tomcat",
+                  "comfortability": 50
+                }
+            ]
+        }
+    }
+})
