e4a91a251105fb7983eb5c43f1806d0ab873b98c
Sanmanyike <terrencemanyike@gmail.com>
2024-05-30 14:23:44 +0200

    Update Invoice Interactive

--- includes/aaee/invoice.js
+++ includes/aaee/invoice.js
@@ -35,5 +35,77 @@ $(document).ready(function() {
     }
     
     document.getElementById('invoice-reference').innerText = generateRandom12DigitNumber();
-    
+
+    // $('#invoice-preview').on('click', function() {
+
+    //     $('#invoice-user-name-gen').text() = "Test Name XXXX";
+    //     console.log("HERE!....");
+    // })
+
+    $('#invoice-preview').on('click', function() {
+        // Capture the values from the form inputs
+        var userName = $('#invoice-user-name').val();
+        var userCompany = $('#invoice-user-company').val();
+        var userAddressOne = $('#user-address-one').val();
+        var userAddressTwo = $('#user-address-two').val();
+        var userEmail = $('#invoice-user-email').val();
+        var userPhone = $('#invoice-user-phone').val();
+
+        var receipientName = $('#invoice-receipient-name').val();
+        var receipientAddressOne = $('#receipient-address-one').val();
+        var receipientAddressTwo = $('#receipient-address-two').val();
+        var receipientEmail = $('#invoice-receipient-email').val();
+        var receipientPhone = $('#invoice-receipient-phone').val();
+
+        var itemNameOne = $('#item-name-one').val();
+        var itemQuantityOne = $('#item-quantity-one').val();
+        var itemPriceOne = $('#item-price-one').val();
+
+        var itemNameTwo = $('#item-name-two').val();
+        var itemQuantityTwo = $('#item-quantity-two').val();
+        var itemPriceTwo = $('#item-price-two').val();
+
+        var vat = $('#vatState').val();
+        var itemOneTotal = itemPriceOne * itemQuantityOne;
+        var itemTwoTotal = itemPriceTwo * itemQuantityTwo;
+        var subTotal = itemOneTotal + itemTwoTotal;
+        var total = subTotal * (vat/100) + subTotal;
+
+        if (itemPriceOne){
+            var itemPriceOne = 'R ' + itemPriceOne;
+        }
+
+        if (itemPriceTwo){
+            var itemPriceTwo = 'R ' + itemPriceTwo;
+        }
+
+        var itemOneTotal = 'R ' + itemOneTotal;
+        var itemTwoTotal = 'R ' + itemTwoTotal;
+
+        // Set the values in the modal
+        $('#modal-user-name').text(userName);
+        $('#modal-user-company').text(userCompany);
+        $('#modal-user-address-one').text(userAddressOne);
+        $('#modal-user-address-two').text(userAddressTwo);
+        $('#modal-user-email').text(userEmail);
+        $('#modal-user-phone').text(userPhone);
+        $('#modal-receipient-name').text(receipientName);
+        $('#modal-receipient-address-one').text(receipientAddressOne);
+        $('#modal-receipient-address-two').text(receipientAddressTwo);
+        $('#modal-receipient-email').text(receipientEmail);
+        $('#modal-receipient-phone').text(receipientPhone);
+        $('#modal-item-name-one').text(itemNameOne);
+        $('#modal-item-quantity-one').text(itemQuantityOne);
+        $('#modal-item-price-one').text(itemPriceOne);
+        $('#modal-item-name-two').text(itemNameTwo);
+        $('#modal-item-quantity-two').text(itemQuantityTwo);
+        $('#modal-item-price-two').text(itemPriceTwo);
+        $('#modal-item-vat').text(vat);
+        $('#modal-item-one-total').text(itemOneTotal);
+        $('#modal-item-two-total').text(itemTwoTotal);
+        $('#modal-item-sub-total').text(subTotal);
+        $('#modal-item-total').text(total);
+
+    });
+
 });
